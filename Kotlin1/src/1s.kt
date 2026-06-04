e9901f1a7add82ccbead13d8aa3093e67feb20ca
Mariyam Aqsa Rafiq <mariyamaqsa.sangamone@gmail.com>
2026-06-04 15:41:07 +0530

    Times in 9 Cities of the world

--- Kotlin1/src/1s.kt
+++ Kotlin1/src/1s.kt
@@ -3,41 +3,15 @@ import java.time.ZoneOffset
 
 fun main() {
 
-    val cities = listOf(
-        "San Francisco",
-        "New York",
-        "London",
-        "Dubai",
-        "Bangalore",
-        "Singapore",
-        "Tokyo",
-        "Sydney",
-        "Wellington"
-    )
-
-    val offsets = listOf(
-        -7.0,
-        -4.0,
-        1.0,
-        4.0,
-        5.5,
-        8.0,
-        9.0,
-        10.0,
-        12.0
-    )
-
+    val cities = listOf("San Francisco", "New York", "London", "Dubai", "Bangalore", "Singapore", "Tokyo", "Sydney", "Wellington")
+    val offsets = listOf(-7.0, -4.0, 1.0, 4.0, 5.5, 8.0, 9.0, 10.0, 12.0)
     val utcTime = LocalDateTime.now(ZoneOffset.UTC)
-
     for (i in cities.indices) {
-
         val hours = offsets[i].toInt()
         val minutes = ((offsets[i] - hours) * 60).toInt()
-
         val localTime = utcTime
             .plusHours(hours.toLong())
             .plusMinutes(minutes.toLong())
-
         println("${cities[i]} : ${localTime.toLocalTime()}")
     }
 }
