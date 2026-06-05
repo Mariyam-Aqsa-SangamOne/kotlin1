9be469c41f4c489e638684f12059203472cf15d6
Mariyam Aqsa Rafiq <mariyamaqsa.sangamone@gmail.com>
2026-06-06 00:18:34 +0530

    Created new list of indices and used shuffle() on it

--- Kotlin1/src/1x.kt
+++ Kotlin1/src/1x.kt
@@ -1,38 +1,47 @@
 import java.io.File
 
-fun quiz1(fname: String){
-    var countries=mutableListOf<String>()
-    var capitals=mutableListOf<String>()
-    var responses=mutableListOf<String>()
-    var marks=mutableListOf<Int>()
-    val f1=File("gk1.txt").readLines()
+fun quiz1(fname: String) {
+    var countries = mutableListOf<String>()
+    var capitals = mutableListOf<String>()
+    var responses = mutableListOf<String>()
+    var marks = mutableListOf<Int>()
+    val f1 = File("gk1.txt").readLines()
 
-    for(i in 0 until 10){
-        val parts=f1[i].trim().split(",")
+    for (i in 0 until 10) {
+        val parts = f1[i].trim().split(",")
         countries.add(parts[0])
         capitals.add(parts[1])
     }
-    for (i in 0 until 10){
-        val index=(0 until 10).random()
+
+    val indices = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    indices.shuffle()
+
+    for (i in 0 until 10) {
+        val index = indices[i]
         println("What is the capital of ${countries[index]}?")
-        val ans=readln()
+        val ans = readln()
         responses.add(ans)
 
-        if(responses[i].trim().lowercase()==capitals[i].trim().lowercase()){
+        if (ans.trim().lowercase() == capitals[index].trim().lowercase()) {
             marks.add(10)
-        }else{
+        } else {
             marks.add(0)
         }
     }
     println()
-    val total=marks.sum()
+    val total = marks.sum()
     println("Total Marks: $total")
-    println("Wrongly Answered Questions:")
-    for (i in 0 until 10){
-        if(marks[i]==0){
-            println("What is the capital of ${countries[i]}?")
-            println("Correct answer: ${capitals[i]}")
-            println()
+    if (total == 100) {
+        println("Congratulations!!")
+    }else {
+        println("Wrongly Answered Questions:")
+        for (i in 0 until 10) {
+            if (marks[i] == 0) {
+                val index = indices[i]
+                println("What is the capital of ${countries[index]}?")
+                println("Correct answer: ${capitals[index]}")
+                println()
+            }
         }
     }
 }
